61ab74c7aaa3841785b8f6151c3e2a6f4056fa44
v.shevelev <v.varitweb@gmail.com>
2020-06-03 13:46:24 +0700

    Get rid of config/services.xml

--- lib/TestServiceBundle/src/DependencyInjection/TestServiceExtension.php
+++ lib/TestServiceBundle/src/DependencyInjection/TestServiceExtension.php
@@ -3,21 +3,21 @@ declare(strict_types=1);
 
 namespace Test\TestServiceBundle\DependencyInjection;
 
-
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Test\TestServiceBundle\TestService;
 
 final class TestServiceExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.xml');
-
         $configuration = $this->getConfiguration($configs, $container);
         $config = $this->processConfiguration($configuration, $configs);
+
+        $container->autowire('test_service_bundle.test_service', TestService::class);
+        $container->setAlias(TestService::class, 'test_service_bundle.test_service');
 
         $definition = $container->getDefinition('test_service_bundle.test_service');
         $definition->setArgument(0, $config['use_expression_language']);
